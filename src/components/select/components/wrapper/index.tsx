26cd69f70b8b2416b80b51432b7ae869b968ef32
VITALIJ <Vitaliythesecond@gmail.com>
2022-02-23 18:38:53 +0300

    wrapper for select

---
+++ src/components/select/components/wrapper/index.tsx
@@ -0,0 +1,47 @@
+import { PropsWithChildren } from "react";
+import styled from "styled-components";
+import { BLACK, RED, disableStyle } from "../../../../assets/tokens";
+
+type TWrapper = {
+    id?: string;
+    className?: string;
+    isError?: boolean;
+    isDisabled?: boolean;
+    width?: string;
+    maxWidth?: string;
+    onClick?: () => void;
+};
+
+const WrapperStyled = styled.div<{ $isError: boolean; $isDisabled: boolean; $width: string; $maxWidth: string }>`
+    width: ${({ $width }) => $width};
+    max-width: ${({ $maxWidth }) => $maxWidth};
+    color: ${({ $isError }) => ($isError ? RED : BLACK)};
+    ${({ $isDisabled }) => $isDisabled && disableStyle};
+`;
+
+const Wrapper = ({
+    id,
+    className,
+    isError = false,
+    isDisabled = false,
+    width = "100%",
+    maxWidth = "100%",
+    children,
+    onClick,
+}: PropsWithChildren<TWrapper>) => {
+    return (
+        <WrapperStyled
+            id={id}
+            className={className}
+            onClick={onClick}
+            $isError={isError}
+            $isDisabled={isDisabled}
+            $width={width}
+            $maxWidth={maxWidth}
+        >
+            {children}
+        </WrapperStyled>
+    );
+};
+
+export default Wrapper;
